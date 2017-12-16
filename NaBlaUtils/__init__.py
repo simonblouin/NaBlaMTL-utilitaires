eddce5c5728d9c197ff9f384812802147052b77c
François Hardy <33529661+Snaptraks@users.noreply.github.com>
2017-12-16 10:27:39 -0500

    Structure pour un package
    Modification de la structure des dossiers pour que ce soit un package de modules.
    
    Inclusion de fortranformat dans le package, afin de ne pas avoir à l'installer séparément.

---
+++ NaBlaUtils/__init__.py
@@ -0,0 +1,8 @@
+"""
+Utilitaires Python pour l'analyse et la visualisation de données relatives aux
+étoiles naines blanches.
+
+Package créé par Simon Blouin en collaboration avec François Hardy
+"""
+
+import fortranformat

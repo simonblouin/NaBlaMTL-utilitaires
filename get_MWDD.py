1435fe26cb5039e6f5fa8478197d83b7aa27dea9
simonblouin <15151278+simonblouin@users.noreply.github.com>
2017-12-18 15:08:25 -0500

    Add files via upload

---
+++ get_MWDD.py
@@ -0,0 +1,12 @@
+import json
+
+def get_MWDD_info(name):
+    """ Retourne un dict avec les parametres de l'etoile donnee en input """
+    data = json.load(open('MWDD_table.json'))
+    for entry in data['data']:
+        try:
+            namelist = entry['allnames']+entry['wdid']+entry['name']
+        except KeyError:
+            namelist = entry['allnames']+entry['wdid']
+        if name.lower() in namelist.lower():
+            return entry

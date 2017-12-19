57303d1419f480182d6614d2c3b396d92d10cfd1
simonblouin <15151278+simonblouin@users.noreply.github.com>
2017-12-19 09:22:52 -0500

    Add files via upload
    Ajouter la possibilité d'extraire des spectres de MWDD.

--- NaBlaUtils/get_MWDD.py
+++ NaBlaUtils/get_MWDD.py
@@ -1,4 +1,5 @@
 import json
+from urllib.request import urlopen
 
 def get_MWDD_info(name):
     """ Retourne un dict avec les parametres de l'etoile donnee en input """
@@ -10,3 +11,43 @@ def get_MWDD_info(name):
             namelist = entry['allnames']+entry['wdid']
         if name.lower().replace(' ', '') in namelist.lower().replace(' ', ''):
             return entry
+
+def get_MWDD_wdid(name):
+    """ Retourne l'identifiant unique d'une etoile dans MWDD """
+    return get_MWDD_info(name)['wdid']
+
+def get_MWDD_spectra(name):
+    """ Retourne un dict contenant tous les spectres de l'etoile demandee dans MWDD """
+    output = {}
+    wdid = get_MWDD_wdid(name)
+    site = 'http://montrealwhitedwarfdatabase.org/WDs/'
+    tar = site+wdid+'/spectlist.txt'
+    tar = tar.replace(' ', '%20')
+    f = str(urlopen(tar).read())
+    f = f.replace(' ', '%20').replace(r'\n', ' ').replace("'", '')
+    listspec = f.split()[1:]
+    for spec in listspec:
+        xvec = []
+        yvec = []
+        s = urlopen(site+wdid.replace(' ', '%20')+'/'+spec).readlines()
+        data = [line.decode("utf-8").strip().split(',')  for line in s[2:]]
+        s[1] = s[1].decode("utf-8").strip()
+        for line in data:
+            if s[1]=='wavelength,flux':
+                x, y = tuple(line)
+                x = float(x)
+                y = float(y)
+            elif s[1]=='wavelength,flux,sigma':
+                x, y, _ = tuple(line)
+                x = float(x)
+                y = float(y)
+            elif s[1]=='wavelength,flux,sigma,flag':
+                x, y, _, _ = tuple(line)
+                x = float(x)
+                y = float(y)
+            else:
+                print('Format inconnu '+spec)
+            xvec.append(x)
+            yvec.append(y)
+        output[spec.replace('%20', '_')] = [xvec,yvec]
+    return output

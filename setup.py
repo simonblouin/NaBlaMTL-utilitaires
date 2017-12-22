a989049f5140ff074756b5ab98eebadb775df63d
François Hardy <33529661+Snaptraks@users.noreply.github.com>
2017-12-22 08:02:27 -0500

    Inclure fichiers de données
    Fix #8

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(name='NaBlaUtils',
       version='0.1',
       description="Utilitaires Python pour l'analyse et la visualisation de données relatives aux étoiles naines blanches",
       url='https://github.com/simonblouin/NaBlaMTL-utilitaires',
+      package_data={'': ['lines.csv', 'MWDD_table.json']},
       packages=find_packages())
 

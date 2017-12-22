902921bdc39ea2e2a2478ad4f6446e0302f6263e
François Hardy <33529661+Snaptraks@users.noreply.github.com>
2017-12-22 08:09:02 -0500

    Renommer fichiers
    Enlève model_analysis du package, et change plot_*.py pour ne pas confondre avec les fonctions du même nom (ça overwrite les packages, et on ne peut plus accéder aux autres fonctions à l'intérieur)

--- NaBlaUtils/__init__.py
+++ NaBlaUtils/__init__.py
@@ -5,6 +5,6 @@ Utilitaires Python pour l'analyse et la visualisation de données relatives aux
 Package créé par Simon Blouin en collaboration avec François Hardy
 """
 
-from .plot_spectre import plot_spectre, load_spectre, load_lines
-from .plot_modele import plot_modele, load_modele
+from .spectres import plot_spectre, load_spectre, load_lines
+from .modeles import plot_modele, load_modele
 from .get_MWDD import get_MWDD_info, get_MWDD_spectra
